e7d35b6558efaa1676b8eca50633a451c55491b1
Deep Karmakar <deepkarmakar61@gmail.com>
2017-01-13 19:10:33 +0530

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -133,3 +133,111 @@ app.service('commoProp', ['$location', '$firebaseAuth', '$state', function($loca
 		}
 	};
 }]);
+
+==================================================================================================
+angular.module('starter.controllers', [])
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('DashCtrl', ['$scope', '$firebaseArray', '$state', '$ionicPopup', 'postService', '$firebase', function($scope, $firebaseArray, $state, $ionicPopup, postService, $firebase){
+  $scope.status = '  ';
+  $scope.customFullscreen = false;
+
+ 
+  // var ref = firebase.database().ref().child('Articles');
+  // $scope.articles = $firebaseArray(ref);
+
+  $scope.articles = postService.getPosts();
+
+}])
+
+.controller('DashSingleCtrl', ['$scope', 'postService', '$stateParams', '$firebase', function($scope, postService, $stateParams, $firebase){
+  var postId = $stateParams.id;
+  $scope.article = postService.getSinglePost(postId); 
+
+  $scope.disableInp = false;
+  $scope.editPost = function(){
+      $scope.disableInp = true;
+  }
+ 
+  // $scope.updateMsg = false; 
+  $scope.updatePost = function(postId){
+    $scope.updateMsg = true;
+    var refercene = firebase.database().ref().child('Articles/' + postId); 
+
+    refercene.update({
+      title : $scope.article.title,
+      post : $scope.article.post
+    }).then(function(refercene){
+      $scope.updateMsg = false; 
+      $scope.updatedMsg = "updated";
+      console.log("done");
+      console.log($scope);
+    }, function(error){
+      console.log(error);
+    });
+  };
+
+}])
+
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller("LoginController", function($scope, $firebaseAuth, $location) {
+ 
+    $scope.login = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            $location.path("/todo");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+ 
+    $scope.register = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$createUser({email: username, password: password}).then(function() {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+            $location.path("/todo");
+        }).catch(function(error) {
+            console.error("ERROR " + error);
+        });
+    }
+ 
+})
+
+.factory('postService', function($firebaseArray, $firebaseObject){
+ 
+  var ref = firebase.database().ref();
+ 
+  return {
+    getPosts: function(){ 
+      return $firebaseArray(ref.child('Articles'));
+    },
+    getSinglePost: function(postId){
+      return $firebaseObject(ref.child('Articles/' + postId));
+    }
+  }  
+});	
